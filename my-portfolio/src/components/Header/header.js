155a6f46cd401cd311dc0845bf72a2302e97da8e
FIRST_NAME LAST_NAME <snieska.juozas@gmail.com>
2023-03-09 23:27:35 +0200

    created Navbar enviroment

---
+++ my-portfolio/src/components/Header/header.js
@@ -0,0 +1,15 @@
+import './header.scss';
+
+function Header() {
+  return (
+    <div className='header'>
+      <nav>
+        <p>test</p>
+        <p>test</p>
+        <p>test</p>
+      </nav>
+    </div>
+  );
+}
+
+export default Header;
--- my-portfolio/src/index.js
+++ my-portfolio/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './app';
+import Header from './components/Header/header';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Header />
     <App />
   </React.StrictMode>
 );
