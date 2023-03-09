2cd423a3287dde43ca48b43e0338adce3b4ebefa
FIRST_NAME LAST_NAME <snieska.juozas@gmail.com>
2023-03-10 00:00:51 +0200

    add elements to the header

--- my-portfolio/src/components/Header/header.js
+++ my-portfolio/src/components/Header/header.js
@@ -3,11 +3,16 @@ import './header.scss';
 function Header() {
   return (
     <div className='header'>
+      <div className='container'>
+      <div className='logo'>
+        <p>My Porfolio by Juzoas</p>
+      </div>
       <nav>
-        <p>test</p>
-        <p>test</p>
-        <p>test</p>
+        <p>Test</p>
+        <p>Test</p>
+        <p>Test</p>
       </nav>
+      </div>
     </div>
   );
 }
