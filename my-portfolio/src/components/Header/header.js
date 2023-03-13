8cd0fba8a2952a52e500ab8e1643e8f9921f8cdb
FIRST_NAME LAST_NAME <snieska.juozas@gmail.com>
2023-03-13 22:41:34 +0200

    added burger and screen sizes to the project

--- my-portfolio/src/components/Header/header.js
+++ my-portfolio/src/components/Header/header.js
@@ -1,16 +1,20 @@
 import './header.scss';
+import { ReactComponent as Burger } from './burger.svg';
 
 function Header() {
   return (
     <div className='header'>
       <div className='container'>
-      <div className='logo'>
-        <p>My Porfolio by Juzoas</p>
-      </div>
-      <nav>
-        <p>About Me</p>
-        <p>Work Expierence</p>
-        <p>Project</p>
+        <div className='logo'>
+          <p>My Porfolio by Juzoas</p>
+        </div>
+        <div className='burger'>
+          <Burger/>
+        </div>
+        <nav>
+          <p>About Me</p>
+          <p>Work Expierence</p>
+          <p>Projects</p>
       </nav>
       </div>
     </div>
